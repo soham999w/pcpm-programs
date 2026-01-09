69afe26e0fbb80dd77397c3aa16dd5efd82ff29a
soham999w <soham1856w@gmail.com>
2026-01-09 20:10:15 +0530

    add new file

---
+++ s2.py
@@ -0,0 +1,29 @@
+# Basics of python
+# variables
+x=5 #integer
+print(x)
+y="Soham" #String
+z=10.5 #float
+print(z)
+k=1j #Complex
+print(k)
+x=bool(5) #Boolean
+# Now we will check the datatypes of variable
+print(type(x)) # Now python will show the datatype of variable example like integer
+# Conversion And Casting of Datatypes
+l=str("3") #python will count as string however it is integer
+w=int(20) # integer
+q=float(7) # float
+# Variable Casting
+int()
+abc=int(1)  #abc will be 1
+cbc=int("1.1") #cbc will be 1
+dec=int(2.8) #dec will be 2.0
+float()
+pap=float(2) # pap will be 2.0
+dad=float(2.8) # dad will be 2.8
+vav=float("4.2") # vav will be 4.2
+str()
+e=str("s1")# e will be 's1'
+r=str(2) # r will bw '2'
+t=str(3.0) # t will be '3.0
