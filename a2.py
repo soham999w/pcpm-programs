407283c5976f940983c34bbac659df7ef0abcc8d
soham999w <soham1856w@gmail.com>
2026-01-10 18:03:44 +0530

    add new file

---
+++ a2.py
@@ -0,0 +1,6 @@
+# Write a python program to check whether a given number is even or odd
+num=int(input("Enter a number:  "))
+if num%2==0:
+    print("Entered number is even...")
+else:
+    print("Entered number is odd...")
