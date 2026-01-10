dd9bf5c78922d8e14801b8112e3d150f5d81fb92
soham999w <soham1856w@gmail.com>
2026-01-10 17:55:18 +0530

    add new file

---
+++ a1.py
@@ -0,0 +1,16 @@
+''' Write a python program to input two numbers and display 
+thier addition,subtraction,multiplication and division'''
+num1=int(input("Enter number 1: "))
+num2=int(input("Enter number 2: "))
+# Now we will add them and display the addition
+add=num1+num2
+print("Addition of two number is:  ",add)
+# Also same thing with subtraction
+sub=num1-num2
+print("Subtraction of two number is:   ",sub)
+# Now multiplication
+mul=num1*num2
+print("Multiplication of two number is: ",mul)
+# Now division
+div=num1/num2
+print("Division of two number is:  ",div)
