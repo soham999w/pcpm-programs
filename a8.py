c207c080584f1742f558ce52ec3b463a4a32026a
soham999w <soham1856w@gmail.com>
2026-01-20 14:14:38 +0530

    add new file

--- a8.py
+++ a8.py
@@ -1,15 +1,16 @@
 ''' Write a python program to reverse a 
 given number and check whether it is a
 palindriome'''
-num=int(input("Enter a number...  "))
-temp=num
-rev=0
-while num>0:
-    digit = num%10
-    rev=rev*10+digit
-    num=num//10
-    print("Reversed number...  ",rev)
-    if temp==rev:
-        print("Palindome number")
-    else:
-        print("Not a palindome number")
+num = int(input("Enter a number: "))
+original_num = num
+reversed_num = 0
+while num > 0:
+    digit = num % 10
+    reversed_num = reversed_num * 10 + digit
+    num //= 10
+
+print("Reversed number:", reversed_num)
+if original_num == reversed_num:
+    print("The number is a palindrome.")
+else:
+    print("The number is not a palindrome.")
